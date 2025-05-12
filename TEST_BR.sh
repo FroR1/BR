f509af1a087956d22320589e3d6e275a62555181
kuyN <108812032+kuyN232@users.noreply.github.com>
2025-05-12 22:03:26 +0700

    Update TEST_BR.sh

--- TEST_BR.sh
+++ TEST_BR.sh
@@ -10,6 +10,13 @@ IP_LAN="192.168.1.1/27"
 DEFAULT_GW="172.16.5.1"
 HOSTNAME="br-rtr.au-team.irpo"
 TIME_ZONE="Asia/Novosibirsk"
+USERNAME="net_admin"
+UID=1010
+PASSWORD="P@$$word"
+BANNER_TEXT="Authorized access only"
+TUNNEL_LOCAL_IP="172.16.5.2"  # IP BR-RTR к ISP
+TUNNEL_REMOTE_IP="172.16.4.2" # IP HQ-RTR к ISP
+TUNNEL_IP="10.0.0.1/30"       # IP для туннеля на BR-RTR
 
 # Функция проверки существования интерфейса
 check_interface() {
@@ -50,6 +57,15 @@ configure_interfaces() {
     ip route add default via "$DEFAULT_GW" dev "$INTERFACE_ISP"
     
     echo "Интерфейсы настроены."
+}
+
+# Функция настройки GRE-туннеля
+configure_tunnel() {
+    echo "Настройка GRE-туннеля..."
+    ip tunnel add gre1 mode gre local "$TUNNEL_LOCAL_IP" remote "$TUNNEL_REMOTE_IP" ttl 64
+    ip addr add "$TUNNEL_IP" dev gre1
+    ip link set gre1 up
+    echo "GRE-туннель настроен."
 }
 
 # Функция настройки nftables и IP forwarding
@@ -107,74 +123,120 @@ set_timezone() {
     echo "Часовой пояс установлен: $TIME_ZONE"
 }
 
-# Функция создания пользователя net_admin
+# Функция создания пользователя
 configure_user() {
-    echo "Создание пользователя net_admin..."
-    if id "net_admin" &> /dev/null; then
-        echo "Пользователь net_admin уже существует."
+    echo "Создание пользователя $USERNAME..."
+    if id "$USERNAME" &> /dev/null; then
+        echo "Пользователь $USERNAME уже существует."
     else
-        useradd -m -u 1010 -s /bin/bash net_admin
-        echo "net_admin:P@$$word" | chpasswd
-        echo "net_admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/net_admin
-        chmod 0440 /etc/sudoers.d/net_admin
-        echo "Пользователь net_admin создан с максимальными привилегиями."
+        useradd -m -u "$UID" -s /bin/bash "$USERNAME"
+        echo "$USERNAME:$PASSWORD" | chpasswd
+        echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/"$USERNAME"
+        chmod 0440 /etc/sudoers.d/"$USERNAME"
+        echo "Пользователь $USERNAME создан с UID $UID и правами sudo."
     fi
+}
+
+# Функция настройки баннера
+configure_banner() {
+    echo "Настройка баннера..."
+    echo "$BANNER_TEXT" > /etc/issue
+    echo "Баннер настроен."
 }
 
 # Функция редактирования данных
 edit_data() {
-    echo "Текущие значения:"
-    echo "1. Интерфейс к ISP: $INTERFACE_ISP"
-    echo "2. Интерфейс к LAN: $INTERFACE_LAN"
-    echo "3. IP для ISP: $IP_ISP"
-    echo "4. IP для LAN: $IP_LAN"
-    echo "5. Шлюз по умолчанию: $DEFAULT_GW"
-    echo "6. Hostname: $HOSTNAME"
-    echo "7. Часовой пояс: $TIME_ZONE"
-    echo "Введите номер параметра для изменения (или 0 для выхода):"
-    read choice
-    case $choice in
-        1) read -p "Новый интерфейс к ISP: " INTERFACE_ISP ;;
-        2) read -p "Новый интерфейс к LAN: " INTERFACE_LAN ;;
-        3) read -p "Новый IP для ISP (например, 172.16.5.2/28): " IP_ISP ;;
-        4) read -p "Новый IP для LAN (например, 192.168.1.1/27): " IP_LAN ;;
-        5) read -p "Новый шлюз по умолчанию: " DEFAULT_GW ;;
-        6) read -p "Новый hostname: " HOSTNAME ;;
-        7) read -p "Новый часовой пояс: " TIME_ZONE ;;
-        0) return ;;
-        *) echo "Неверный выбор." ;;
-    esac
+    while true; do
+        clear
+        echo "Текущие значения:"
+        echo "1. Интерфейс к ISP: $INTERFACE_ISP"
+        echo "2. Интерфейс к LAN: $INTERFACE_LAN"
+        echo "3. IP для ISP: $IP_ISP"
+        echo "4. IP для LAN: $IP_LAN"
+        echo "5. Шлюз по умолчанию: $DEFAULT_GW"
+        echo "6. Hostname: $HOSTNAME"
+        echo "7. Часовой пояс: $TIME_ZONE"
+        echo "8. Имя пользователя: $USERNAME"
+        echo "9. UID пользователя: $UID"
+        echo "10. Пароль пользователя: $PASSWORD"
+        echo "11. Текст баннера: $BANNER_TEXT"
+        echo "12. Локальный IP для туннеля: $TUNNEL_LOCAL_IP"
+        echo "13. Удаленный IP для туннеля: $TUNNEL_REMOTE_IP"
+        echo "14. IP для туннеля: $TUNNEL_IP"
+        echo "0. Назад"
+        echo "Введите номер параметра для изменения (или 0 для выхода):"
+        read choice
+        case $choice in
+            1) read -p "Новый интерфейс к ISP [$INTERFACE_ISP]: " input
+               INTERFACE_ISP=${input:-$INTERFACE_ISP} ;;
+            2) read -p "Новый интерфейс к LAN [$INTERFACE_LAN]: " input
+               INTERFACE_LAN=${input:-$INTERFACE_LAN} ;;
+            3) read -p "Новый IP для ISP [$IP_ISP]: " input
+               IP_ISP=${input:-$IP_ISP} ;;
+            4) read -p "Новый IP для LAN [$IP_LAN]: " input
+               IP_LAN=${input:-$IP_LAN} ;;
+            5) read -p "Новый шлюз по умолчанию [$DEFAULT_GW]: " input
+               DEFAULT_GW=${input:-$DEFAULT_GW} ;;
+            6) read -p "Новый hostname [$HOSTNAME]: " input
+               HOSTNAME=${input:-$HOSTNAME} ;;
+            7) read -p "Новый часовой пояс [$TIME_ZONE]: " input
+               TIME_ZONE=${input:-$TIME_ZONE} ;;
+            8) read -p "Новое имя пользователя [$USERNAME]: " input
+               USERNAME=${input:-$USERNAME} ;;
+            9) read -p "Новый UID пользователя [$UID]: " input
+               UID=${input:-$UID} ;;
+            10) read -p "Новый пароль пользователя [$PASSWORD]: " input
+                PASSWORD=${input:-$PASSWORD} ;;
+            11) read -p "Новый текст баннера [$BANNER_TEXT]: " input
+                BANNER_TEXT=${input:-$BANNER_TEXT} ;;
+            12) read -p "Новый локальный IP для туннеля [$TUNNEL_LOCAL_IP]: " input
+                TUNNEL_LOCAL_IP=${input:-$TUNNEL_LOCAL_IP} ;;
+            13) read -p "Новый удаленный IP для туннеля [$TUNNEL_REMOTE_IP]: " input
+                TUNNEL_REMOTE_IP=${input:-$TUNNEL_REMOTE_IP} ;;
+            14) read -p "Новый IP для туннеля [$TUNNEL_IP]: " input
+                TUNNEL_IP=${input:-$TUNNEL_IP} ;;
+            0) return ;;
+            *) echo "Неверный выбор." ;;
+        esac
+    done
 }
 
 # Основное меню
 while true; do
+    clear
     echo -e "\nМеню настройки BR-RTR:"
     echo "1. Редактировать данные"
     echo "2. Настроить сетевые интерфейсы"
     echo "3. Настроить NAT и IP forwarding"
-    echo "4. Установить hostname"
-    echo "5. Установить часовой пояс"
-    echo "6. Настроить пользователя net_admin"
-    echo "7. Выполнить все настройки"
-    echo "8. Выход"
+    echo "4. Настроить GRE-туннель"
+    echo "5. Установить hostname"
+    echo "6. Установить часовой пояс"
+    echo "7. Настроить пользователя"
+    echo "8. Настроить баннер"
+    echo "9. Выполнить все настройки"
+    echo "10. Выход"
     read -p "Выберите опцию: " option
     
     case $option in
         1) edit_data ;;
         2) configure_interfaces ;;
         3) configure_nftables ;;
-        4) set_hostname ;;
-        5) set_timezone ;;
-        6) configure_user ;;
-        7) 
+        4) configure_tunnel ;;
+        5) set_hostname ;;
+        6) set_timezone ;;
+        7) configure_user ;;
+        8) configure_banner ;;
+        9) 
             configure_interfaces
             configure_nftables
+            configure_tunnel
             set_hostname
             set_timezone
             configure_user
+            configure_banner
             echo "Все настройки выполнены."
             ;;
-        8) echo "Выход."; exit 0 ;;
+        10) echo "Выход."; exit 0 ;;
         *) echo "Неверный выбор." ;;
     esac
 done
